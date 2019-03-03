46f0c7d3957eb1fc3289031d8d28d415bdbd95ce
Clement Truillet <clem.truillet@gmail.com>
2019-03-03 18:50:40 +0100

    Add content in encrypt.c

--- include/encrypt.h
+++ include/encrypt.h
@@ -4,7 +4,7 @@
 #define __ENCRYPT_H__
 
 #ifndef __USER_ACCOUNT_H__
-	#include "../include/users_account.h"
+	#include "../include/user_account.h"
 #endif
 
 /*
@@ -13,6 +13,8 @@
 */
 
 //Check if the password is good
+//	Return 1 if password is good
+//	Return 0 if ... well.. is false ?
 int passwordIsGood(char * password, user_account ua);
 
 //Encrypt password
--- src/encrypt.c
+++ src/encrypt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/user_account.h"
 
 /*
@@ -9,22 +10,28 @@
 
 //Check if the password is good
 int passwordIsGood(char * password, user_account ua){
-    /*
-    * Recupere la struct de l'user entré
-    * encyryptPassword(password)
-    * strcmp
-    *   -> == 0 OK
-    *   -> != 0 Pas Ok -> error
-    */
-    return 0;
+    char * user_pwd = get_pwd(ua);
+    return (strcmp(password,user_pwd)==0 ? 1 : 0);
 }
 
 //Encrypt password
 char * encryptPassword(char * string){
-    /*
-    * Recupere string
-    * md5
-    * renvoit md5(string)
-    */
-    return 'c';
+    FILE *fp;
+    char path[1035];
+    char password[64]; //for the encryp password
+    char chaine[128] = "echo "; 
+    char end_chaine[128] = " | openssl md5 | cut -d\" \" -f2"; 
+    //Create the command
+    strcat(chaine,string); 
+    strcat(chaine,end_chaine); 
+    //Execute
+    fp = popen(chaine, "r");
+    //Reclaim the encrypt password
+    fgets(path, sizeof(path)-1, fp);
+    pclose(fp);
+    //To remove the caracter '\n'
+    strncpy(password,path,strlen(path)-1);
+
+    //OK IT'S FINISH !
+    return password;
 }
