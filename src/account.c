dd3856f128e46ab33739ec13a2e6bec94659a606
vfrydrychowski <44438290+vfrydrychowski@users.noreply.github.com>
2019-03-16 14:38:37 +0100

    attempt to fix pull

--- src/account.c
+++ src/account.c
@@ -21,8 +21,8 @@ typedef struct account_s
 //structure of an account list
 typedef struct list_account_s
 {
-    account current; //current element of the list
-    list_account *next;   //next element of the list
+    Account current; //current element of the list
+    List_account next;   //next element of the list
 } list_account;
 
 //getters
@@ -42,9 +42,10 @@ float get_balance(Account acc){
     //TODO
 }
 
-char **get_history(Account acc, char date1, char date2)[
+char **get_history(Account acc, char date1, char date2){
+
     //TODO  
-]
+}
 
 //setters
 
@@ -52,14 +53,25 @@ void add_history(Account acc, char *operation){
     //TODO
 }
 
-void set_owners(Account acc, char **owners){
-    acc->owners=owners;
+void set_owners(Account acc, char **own){
+    acc->owners[0]=own[0];
+    acc->owners[1]=own[1];
 }
 
-void set_ID(Account acc, char ID){
+void set_ID(Account acc, char *ID){
     acc->ID=ID;
 }
 
-void set_type_account(Account acc, char type){
+void set_type_account(Account acc, char *type){
     acc->type_account = type;
+}
+
+void add_list(List_account l, Account acc){
+    while (l != NULL){
+        l=l->next;
+    }
+    List_account l2;
+    l2->current = acc;
+    l2->next = NULL;
+    l=l2;
 }
--- src/user_account.c
+++ src/user_account.c
@@ -2,88 +2,93 @@
 * Valentin Frydrychowski 
 * Derniere modification : 03/03/2019
 */
-
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "../include/user_account.h"
 #include "../include/account.h"
 
-
 //structure from a user account
-typedef struct user_account_s{
-    char *u_ID;     //ID of .json file
-    bool admin;     //account with admin rights
-    char *name;     //name of user
-    char *firstname;        //first name of user
-    char *pwd;      //password link to the account
+typedef struct user_account_s
+{
+    char *u_ID;        //ID of .json file
+    bool admin;        //account with admin rights
+    char *name;        //name of user
+    char *firstname;   //first name of user
+    char *pwd;         //password link to the account
     List_account list; //list of account own by user
 } user_account;
 
 //getters
-bool is_admin(User_account uacc){
+bool is_admin(User_account uacc)
+{
     return uacc->admin;
-
 }
 
-char *get_u_ID(User_account uacc){
+char *get_u_ID(User_account uacc)
+{
     return uacc->u_ID;
 }
 
-char *get_name(User_account uacc){
+char *get_name(User_account uacc)
+{
     return uacc->name;
 }
 
-char *get_firstname(User_account uacc){
+char *get_firstname(User_account uacc)
+{
     return uacc->firstname;
 }
 
-char *get_pwd(User_account uacc){
+char *get_pwd(User_account uacc)
+{
     return uacc->pwd;
 }
 
-char **get_account_list(User_account uacc){
+char **get_account_list(User_account uacc)
+{
     //TODO toString
 }
 
 //Setters
 
-void set_admin(User_account uacc, bool admin){
-    uacc->admin=admin;
+void set_admin(User_account uacc, bool admin)
+{
+    uacc->admin = admin;
 }
 
-void set_name(User_account uacc, char name){
-    uacc->name=name;
+void set_name(User_account uacc, char *name)
+{
+    uacc->name = name;
 }
 
-void set_firstname(User_account uacc, char firstname){
-    uacc->firstname=firstname;
+void set_firstname(User_account uacc, char *firstname)
+{
+    uacc->firstname = firstname;
 }
 
-void set_pwd(User_account uacc, char pwd){
-    uacc->pwd=pwd;
+void set_pwd(User_account uacc, char *pwd)
+{
+    uacc->pwd = pwd;
 }
 
-void add_Ulist(User_account uacc, Account acc){
-    void add_list(List_account l, Account acc){
-        if (l!=NULL)
-            add_list(l->next, acc);
-        List_account l2;
-        l2->current=acc;
-        l2->next=NULL:
-    }
-    add_list(uacc->list, Account acc);
-}b 
+void add_Ulist(User_account uacc, Account acc)
+{
+    add_list(uacc->list, acc);
+}
 
 //file management
 
-create_user_account(bool admin, char *name, char *firstname, char *pwd, List_account list){
-    char U_id=create_user_ID(name);
+User_account create_user_account(bool admin, char *name, char *firstname, char *pwd, List_account list)
+{
+    char *U_id = create_user_ID(name);
     //creation and opening of json
-    FILE *json = fopen(U_id, w+);
+    FILE *json = fopen(U_id, "w+");
     //filing of json with the json structure
-    fprintf(json, "{\n\t\"user_account\": {\n\t\t\"ID\": \"%s\",\n\t\t\"admin\": \"%s\",\n\t\t\"firtname\": \"%s\",\n\t\t\"pwd\": \"%s\",\n\t\t\"List_account\": \"%s\"\n\t}\n}",U_id,admin,firstname,pwd,List_accountToString(list));
-    User_account uacc=malloc(sizeof(User_account));
+    fprintf(json, "{\n\t\"user_account\": {\n\t\t\"ID\": \"%s\",\n\t\t\"admin\": \"%s\",\n\t\t\"firstname\": \"%s\",\n\t\t\"name\": \"%s\",\n\t\t\"pwd\": \"%s\",\n\t\t\"List_account\": \"%s\"\n\t}\n}", U_id, admin, firstname, name, pwd, List_accountToString(list));
+    fclose(json);
+    User_account uacc = NULL;
     charge_user_account(uacc, U_id);
-    return U_id;
+    return uacc;
 }
