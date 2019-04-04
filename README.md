a2976219c2c429858b1c7e19fd44d755e4deac03
Clement Truillet <clem.truillet@gmail.com>
2019-04-04 08:59:07 +0200

    👌 IMPROVE: README

--- README.md
+++ README.md
@@ -1,40 +1,17 @@
-## Welcome on KreGit bank
+# Welcome on KreGit bank
 
 We are very happy to see you in your website because you're our favorite customer !  
 So, you win a free download link to our github, because it's poor students who had develop this project.   
   
-Well, I think you don't have finish our project yet, come back soon !
+Well, I think we don't have finish our project yet, come back soon !
 
 ![Perceval, the best man the earth has ever had](https://raw.githubusercontent.com/ClementTruillet/KreGit/master/doc/Perceval.png)   
 Perceval, the best man the earth has ever had
-### Markdown
 
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
+## Documentation and rapport
 
-```markdown
-Syntax highlighted code block
+Not yet !
 
-# Header 1
-## Header 2
-### Header 3
+## Contact
 
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
-```
-
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/ClementTruillet/KreGit/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
-### Support or Contact
-
-Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
+Contact us by email : *clement.truillet@univ-tlse3.fr* or *valentin.frydrychowski@univ-tlse3.fr*.  
--- src/account.c
+++ src/account.c
@@ -25,139 +25,139 @@
  * 
  */
 typedef struct account_s{
-    char ID[64];                //Identification
-    char type_account[32];      //Type of account
-    struct account_s* next;     //Next Account
+	char ID[64];                //Identification
+	char type_account[32];      //Type of account
+	struct account_s* next;     //Next Account
 } account ;
 
 //Getters
 char * get_id(Account acc){
-    return acc->ID;
+	return acc->ID;
 }
 char * get_type_account(Account acc){
-    return acc->type_account;
+	return acc->type_account;
 }
 
 Account getNextAccount(Account ac){
-    return ac->next;
+	return ac->next;
 }
 
 //Setters
 void set_ID(Account acc, char * ID){
-    strcpy(acc->ID,ID);
+	strcpy(acc->ID,ID);
 }
 
 void set_type_account(Account acc, char * type){
-    strcpy(acc->type_account,type);
+	strcpy(acc->type_account,type);
 }
 
 void InfoAccount(Account a){
-    printf("--------------------------\n| Compte %s\n| \tType : %s\n| \tSuivant ? %s\n--------------------------\n",get_id(a),get_type_account(a),(getNextAccount(a)!=NULL)?"Oui":"Non");
+	printf("--------------------------\n| Compte %s\n| \tType : %s\n| \tSuivant ? %s\n--------------------------\n",get_id(a),get_type_account(a),(getNextAccount(a)!=NULL)?"Oui":"Non");
 }
 
 Account setAccount(char * ID){
-    Account ac = (Account) malloc(sizeof(struct account_s));
-    set_ID(ac,ID);
-    char temp[32]= "\0";
+	Account ac = (Account) malloc(sizeof(struct account_s));
+	set_ID(ac,ID);
+	char temp[32]= "\0";
 
-    strcpy(temp,ID);
-    // Get the first token
-    char *type = strtok(temp,"-"); // merdier token
-    set_type_account(ac,type);
-    ac->next=NULL;
-    return (ac);
+	strcpy(temp,ID);
+	// Get the first token
+	char *type = strtok(temp,"-"); // merdier token
+	set_type_account(ac,type);
+	ac->next=NULL;
+	return (ac);
 }
 
 Account addNewAccount(Account a, Account aAdd){
-    if(a==NULL){
-        a=aAdd;
-    }else{
-        Account ac = a;
-        while (ac->next != NULL){
-            //printf(">> on passe le Compte %s\n",get_id(ac));
-            ac = getNextAccount(ac);
-        }
-        ac->next = aAdd;
-    }
-    return a;
+	if(a==NULL){
+		a=aAdd;
+	}else{
+		Account ac = a;
+		while (ac->next != NULL){
+			//printf(">> on passe le Compte %s\n",get_id(ac));
+			ac = getNextAccount(ac);
+		}
+		ac->next = aAdd;
+	}
+	return a;
 }
 
 
 int nbrAccount(Account a){
-    if(a == NULL){
-        return 0;
-    }else{
-        int size = 0;
-        Account ac = a;             //to avoid modification on the pointeur
-        while (ac != NULL){         //while we didn't reach the end of the list
-            size++;
-            ac = ac->next;
-        }
-        return size;
-    }
+	if(a == NULL){
+		return 0;
+	}else{
+		int size = 0;
+		Account ac = a;             //to avoid modification on the pointeur
+		while (ac != NULL){         //while we didn't reach the end of the list
+			size++;
+			ac = ac->next;
+		}
+		return size;
+	}
 }
 
 char *List_accountToString(Account a){
-    Account a2 = a;                                     //to avoid modification on the pointeur
-    char * strR = (char *)malloc(sizeof(char)*128);
-    char str[128] = "[";
-    while(a2!=NULL){
-        strcat(str,"\"");
-        strcat(str,a2->ID);
-        strcat(str,"\"");
-        if(a2->next != NULL){
-            strcat(str,",");
-        }
-        a2 = a2->next;
-    }
-    strcat(str,"]");
-    strcpy(strR,str);
-    return strR;
+	Account a2 = a;                                     //to avoid modification on the pointeur
+	char * strR = (char *)malloc(sizeof(char)*128);
+	char str[128] = "[";
+	while(a2!=NULL){
+		strcat(str,"\"");
+		strcat(str,a2->ID);
+		strcat(str,"\"");
+		if(a2->next != NULL){
+			strcat(str,",");
+		}
+		a2 = a2->next;
+	}
+	strcat(str,"]");
+	strcpy(strR,str);
+	return strR;
 }
 
 //file management
 char * createAccountID(char * type){
-    char *ID = (char*)malloc(32);
-    char *timeS = (char*)malloc(32);
-    time_t temps;
-    struct tm date;
+	char *ID = (char*)malloc(32);
+	char *timeS = (char*)malloc(32);
+	time_t temps;
+	struct tm date;
 
-    // Recuperation de la date et l'heure actuelle.
-    time(&temps);
-    date=*localtime(&temps);
+	// Recuperation de la date et l'heure actuelle.
+	time(&temps);
+	date=*localtime(&temps);
 
-    // Remplissage de la chaîne avec en date_heure
-    strftime(timeS, 128, "%m%d%Y%H%M%S", &date);
-    sprintf(ID,"%s-%s",type,timeS);
-    
-    return ID;
+	// Remplissage de la chaîne avec en date_heure
+	strftime(timeS, 128, "%m%d%Y%H%M%S", &date);
+	sprintf(ID,"%s-%s",type,timeS);
+	
+	return ID;
 }
 
 void createAccountCsv(char * ID){
-    char path[64];
-    sprintf(path,"data/account/%s.csv",ID);
+	char path[64];
+	sprintf(path,"data/account/%s.csv",ID);
 
-    FILE * fileCSV = fopen(path, "a");
-    if (fileCSV != NULL){
-        fprintf(fileCSV, "date,operation,solde,comments");
-        fclose(fileCSV);
-    }
+	FILE * fileCSV = fopen(path, "a");
+	if (fileCSV != NULL){
+		fprintf(fileCSV, "date,operation,solde,comments");
+		fclose(fileCSV);
+	}
 }
 
 int newOperation(Account a, double operation, char * comment){
-    /* ToDo
-    *   Ouvrir le fichier CSV associé
-    *   Faire l'opération
-    *   Mettre a jour le solde
-    */
-    return 0;
+	/* ToDo
+	*   Ouvrir le fichier CSV associé
+	*   Faire l'opération
+	*   Mettre a jour le solde
+	*/
+	return 0;
 }
 
 Account createAccount(char * ID, char * type_account){
-    Account acc = (Account)malloc(sizeof(struct account_s));
-    set_ID(acc,ID);
-    set_type_account(acc,type_account);
-    acc->next=NULL;
+	Account acc = (Account)malloc(sizeof(struct account_s));
+	set_ID(acc,ID);
+	set_type_account(acc,type_account);
+	acc->next=NULL;
 
-    return acc;
+	return acc;
 } 
